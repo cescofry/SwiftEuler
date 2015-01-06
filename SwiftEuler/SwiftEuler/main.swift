71cf2f6d3b3810147c1e7a7a43a13fc3636f8151
Francesco Frison <ffrison@yammer-inc.com>
2015-01-06 14:37:07 +0000

    switch to command line project

---
+++ SwiftEuler/SwiftEuler/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  SwiftEuler
+//
+//  Created by Francesco Frison on 06/01/2015.
+//  Copyright (c) 2015 Ziofrtiz. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
