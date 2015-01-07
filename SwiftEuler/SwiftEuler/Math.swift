6424bd8b35d3b202d7c0681f3a01ff05e3e0e79e
Francesco Frison <ffrison@yammer-inc.com>
2015-01-07 14:17:26 +0000

    exercise 11 & 12

--- SwiftEuler/SwiftEuler/Math.swift
+++ SwiftEuler/SwiftEuler/Math.swift
@@ -9,6 +9,11 @@
 import Foundation
 
 
+func sqrt(x: Int) -> Int {
+    return Int(sqrt(Float(x)))
+}
+
+
 struct Primes : GeneratorType {
     
     typealias Element = Int
@@ -44,5 +49,38 @@ struct Primes : GeneratorType {
                 return next
             }
         }
+    }
+}
+
+struct Triangles : GeneratorType {
+    
+    typealias Element = Int
+    
+    var primes : [Int] = Array()
+    
+    mutating func next() -> Element? {
+        
+        if primes.count == 0 {
+            primes.append(1)
+            return 1
+        }
+        
+        let last = primes.last!
+        let next = last + primes.count + 1
+        primes.append(next)
+        
+        return next
+    }
+}
+
+
+func parseInput(input: String) {
+    var lines = input.componentsSeparatedByString("|")
+    
+    for (index, line) in enumerate(lines) {
+        let lineCommas = line.stringByReplacingOccurrencesOfString(" ", withString: ", ", options: NSStringCompareOptions.CaseInsensitiveSearch, range: nil)
+        let varLine = "matrix.append([\(lineCommas)])"
+        
+        println(varLine)
     }
 }
