43905e8b5984b8a4ee45c8bea44fac7cb9196e48
Francesco Frison <cescofry@gmail.com>
2015-01-07 00:22:01 +0000

    8 and 9

--- SwiftEuler/SwiftEuler/euler0-10.swift
+++ SwiftEuler/SwiftEuler/euler0-10.swift
@@ -142,11 +142,62 @@ class Exercise0To10: ExerciseGroupable {
         return String(number)
     }
     
-    var exercise8 = Exercise(description: "") { () -> (String) in
-        return ""
+    var exercise8 = Exercise(description: "The four adjacent digits in the 1000-digit number that have the greatest product are 9 × 9 × 8 × 9 = 5832. Find the thirteen adjacent digits in the 1000-digit number that have the greatest product. What is the value of this product?") { () -> (String) in
+        
+        let input = Array( "7316717653133062491922511967442657474235534919493496983520312774506326239578318016984801869478851843858615607891129494954595017379583319528532088055111254069874715852386305071569329096329522744304355766896648950445244523161731856403098711121722383113622298934233803081353362766142828064444866452387493035890729629049156044077239071381051585930796086670172427121883998797908792274921901699720888093776657273330010533678812202354218097512545405947522435258490771167055601360483958644670632441572215539753697817977846174064955149290862569321978468622482839722413756570560574902614079729686524145351004748216637048440319989000889524345065854122758866688116427171479924442928230863465674813919123162824586178664583591245665294765456828489128831426076900422421902267105562632111110937054421750694165896040807198403850962455444362981230987879927244284909188845801561660979191338754992005240636899125607176060588611646710940507754100225698315520005593572972571636269561882670428252483600823257530420752963450" )
+        
+        let limit = input.count - 13
+        var index = 0
+        var result = 0
+        while index < limit {
+            var tot = 1
+            for subIndex in index...(index+12) {
+                let num = String(input[subIndex]).toInt()!
+                if (num == 0) {
+                    index++
+                    tot = 0
+                    break
+                }
+                tot *= num
+            }
+            
+            if (tot > result) {
+                println(tot)
+                result = tot
+            }
+            
+            index++
+        }
+        
+        
+        
+        return String(result);
     }
     
-    var exercise9 = Exercise(description: "") { () -> (String) in
+    var exercise9 = Exercise(description: "A Pythagorean triplet is a set of three natural numbers, a < b < c, for which, a2 + b2 = c2 For example, 32 + 42 = 9 + 16 = 25 = 52. There exists exactly one Pythagorean triplet for which a + b + c = 1000. Find the product abc.") { () -> (String) in
+        
+        func isTriplet(#a: Int, b: Int, c: Int) -> Bool {
+            return (a * a) + (b * b) == (c * c)
+        }
+        
+        
+        var a = 1
+        var b = 1
+        var c = 1
+        
+        let limit = 500
+        
+        for a in 1...limit {
+            for b in a...limit {
+                for c in b...limit {
+                    if isTriplet(a: a, b, c) && a + b + c == 1000 {
+                        println("\(a)  \(b) \(c)")
+                        return String(a * b * c)
+                    }
+                }
+            }
+        }
+        
         return ""
     }
     
